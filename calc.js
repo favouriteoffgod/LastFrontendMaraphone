3d30cff3871eb384e2c7c96dd3dc22d9fe0be48c
Tatiana Ershova <ershovati24@gmail.com>
2022-02-01 10:14:51 +0300

    Refactoring after learn JS-objects

--- calc.js
+++ calc.js
@@ -4,35 +4,24 @@ function calc(operator, a, b) {
   const isNotCorrect = (!a || !b || !operator
     || typeof a !== "number" || typeof b !== "number");
 
-    if (isNotCorrect) {
-        result = "Error";
-    } else {
-        switch (operator) {
-            case "sum":
-                result = a + b;
-                break;
-            case "sub":
-                result = a - b;
-                break;
-            case "multi":
-                result = a * b;
-                break;
-            case "div":
-                result = a / b;
-                break;
-            case "exp":
-                result = a ** b;
-                break;
-            
-            default: 
-                result = "Unknown operation";
-                break;
-        }
+   const operations = {
+        sum: (a + b),
+        sub: (a - b),
+        mult: (a * b),
+        div: (a / b),
+        exp: (a ** b),
     }
-    return result;
+
+    if (isNotCorrect) {
+       return "Error";
+    } else if (operator in operations) {
+        return operations[operator];
+    } else {
+        return "Unknown operator";
+    }
 }
 
-console.log( calc("multi", 4, 5) );
+console.log( calc("mult", 4, 5) );
 console.log( calc("sum", 5) );
 console.log( calc("nuude", 6, 7) );
 console.log( calc("div", 3, true) );
